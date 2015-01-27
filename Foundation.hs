a876c4484b0658177b37c3e5a824fde7a6732bf2
thelollies <rorystephenson@gmail.com>
2015-01-27 22:26:04 +1300

    Replace bootstrap for min css framework

--- Foundation.hs
+++ Foundation.hs
@@ -59,7 +59,7 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
+            addStylesheetRemote $ "//cdn.jsdelivr.net/min/1.5/min.min.css"
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
